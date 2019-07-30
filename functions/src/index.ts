511bee94310aaf1b9086eaccfa685ac61d32561e
Fahd <fahd.boudali@nordnet.com>
2019-07-30 17:15:55 +0200

    inserting data into database

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,25 @@
-//import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+admin.initializeApp();
+
+/**
+ * Insert Raw Json into database or throw a 500 error
+ */
+export const dbInsertFunction = functions.https.onRequest((request, response) => {
+    admin
+        .firestore()
+        .collection("inputs")
+        .add(
+            request.body
+        )
+        .then(
+            () => response.send(request.body + 'inserted')
+        )
+        .catch(
+            () => response.sendStatus(500)
+        );
+});
