33b11cc9ad1388b6ff528d73e925a4879dcefa7c
Aloulou Amine <amine.aloulou.ext@nordnet.fr>
2019-09-03 04:52:03 +0200

    enhance toolbar.

--- fiber-tools-ui/src/app/app.module.ts
+++ fiber-tools-ui/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
@@ -40,7 +41,8 @@ import { MatListModule } from '@angular/material/list';
     MatIconModule,
     MatButtonModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatMenuModule
   ],
   providers: [
   ],
