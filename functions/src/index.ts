c990b087ace41fd83f612aca475f35775b8d45f2
Aloulou Amine <amine.aloulou.ext@nordnet.fr>
2019-10-20 11:09:28 +0200

    add archiving function hook.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,6 @@
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
+import { DocumentSnapshot } from 'firebase-functions/lib/providers/firestore';
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
@@ -36,3 +37,14 @@ export const deleteUser = functions.auth.user().onDelete(user => {
         .then(() => console.log(`user ${user.email} deleted.`))
         .catch(err => console.error('error deleting user ', err))
 });
+
+/**
+ * TODO : Archiving here. 
+ */
+export const cleanUpMissions = functions.firestore
+    .document('sites/{siteId}')
+    .onDelete((snapshot: DocumentSnapshot) => snapshot.ref
+        .collection('missions')
+        .listDocuments()
+        .then(doc => doc.forEach(d => d.delete()))
+    );
