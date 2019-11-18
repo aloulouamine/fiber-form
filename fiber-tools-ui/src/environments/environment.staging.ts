bb477fe4bf4974ed7ffccb56865891e24cf0c5d0
Aloulou Amine <amine.aloulou.ext@nordnet.fr>
2019-11-18 18:51:25 +0100

    add staging environment.

---
+++ fiber-tools-ui/src/environments/environment.staging.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAuJXzR0o0F5OtTKRxzdXZQpwfSy1r7nCE",
+    authDomain: "fiber-tools-staging.firebaseapp.com",
+    databaseURL: "https://fiber-tools-staging.firebaseio.com",
+    projectId: "fiber-tools-staging",
+    storageBucket: "fiber-tools-staging.appspot.com",
+    messagingSenderId: "282345999686",
+    appId: "1:282345999686:web:c148250d281f0adb0b6947",
+    measurementId: "G-ZV1ZZ2J4JT"
+  }
+};
